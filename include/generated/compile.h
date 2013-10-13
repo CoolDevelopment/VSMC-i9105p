efa0548fbd50e52ca6192e4951070cf861c88d46
Cool Development <paulimaen@hotmail.de>
2013-10-13 15:06:56 +0200

    first commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Wed Jul 24 22:18:41 EDT 2013"
+#define LINUX_COMPILE_BY "koo"
+#define LINUX_COMPILE_HOST "koo-desktop"
+#define LINUX_COMPILER "gcc version 4.6.x-google 20120106 (prerelease) (GCC) "
